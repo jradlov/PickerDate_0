a126aa884bf13047b54bbf243b0a485e14b74019
Jaime Radulovich <jradlov@gmail.com>
2018-06-08 09:38:01 -0700

    Add project files.

---
+++ PickerDate_0/PickerDate_0/MainPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace PickerDate_0
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+		}
+
+        private void DatePicker_DateSelected(object sender, DateChangedEventArgs e)
+        {
+            DisplayAlert("Date Selected", e.NewDate.ToLongDateString(), "Ok");
+        }
+
+        private void TimePicker_PropertyChanged(object sender, System.ComponentModel.PropertyChangedEventArgs e)
+        {
+            string time = timePicker.Time.ToString();
+            DisplayAlert("time Selected", time, "Ok");
+        }
+    }
+}
